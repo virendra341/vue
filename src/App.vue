4b0cb40351f1a92f21485b1ba08c35627f2fb036
virendra patidar <virendra.patidar@47billion.com>
2019-04-17 16:39:29 +0530

    Sample project vuew

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+<div>
+   <todo-list v-bind:todos="todos"></todo-list>
+   <create-todo  v-on:create-todo="createTodo"/>
+</div>
+
+</template>
+
+<script>
+import sweetalert from 'sweetalert';
+import TodoList from './components/TodoList'
+import CreateTodo from './components/CreateTodo'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTodo
+  },
+  data () {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+        isEditing:true
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+        isEditing:false
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+        isEditing:true
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+        isEditing:false
+      }]
+    }
+  },
+  methods:{
+    createTodo(newTodo){
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo  v-on:delete-todo="deleteConfirmBox" v-for="todo in todos" v-bind:todo="todo" v-bind:key="todo.project"></todo>
+  </div>
+</template>
+<script type = "text/javascript" >
+import sweetalert from 'sweetalert';
+import Todo from './Todo';
+export default {
+  name: 'TodoList',
+  props: ['todos'],
+   components: {
+    Todo,
+  },
+  methods:{
+    deleteTodo(todo){
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    deleteConfirmBox(){
+      sweetalert("Are you sure you want to delete this to do?", {
+        buttons: ["No!", "Yes!"],
+      });
+    }
+
+  }
+}
+</script>
